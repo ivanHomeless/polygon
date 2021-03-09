0b773396c97951dcad76cc76210e6d048182a55a
Dmitry <double0fork@gmail.com>
2021-03-09 21:01:45 +0300

    Category

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * Class BlogCategory
  * @package App\Models
  * @mixin Eloquent
+ * @property $id
  * @property $title
  * @property $slug
  *
@@ -19,13 +20,36 @@ class BlogCategory extends Model
 {
     use SoftDeletes;
 
+    const ROOT = 1;
+
     protected $fillable = [
         'title',
         'slug',
         'parent_id',
         'description',
     ];
+
     /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      * @var mixed|string
      */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+        ?? ($this->isRoot()
+            ? 'Корень'
+            : '???');
+
+        return $title;
+    }
+
+    protected function isRoot()
+    {
+        return $this->id == self::ROOT;
+    }
 }
